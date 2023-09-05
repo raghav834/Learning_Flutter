81bf9bb315e18f068ca7485ac8c6f65909abe397
RaT332 <rahultekchandani323@gmail.com>
2023-09-05 18:56:26 +0530

    Stack Widget Example with Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,18 +9,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
         home:Scaffold(
           body: Stack(
             children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              const Text("Hello")
+               Positioned(
+                left: 30,
+                 top: 100,
+                 child:  Container(
+                   height: 80,
+                   width: 260,
+                   color: Colors.blueGrey,
+                   alignment: Alignment.center,
+                   transform: Matrix4.rotationZ(-0.25),
+                   child: const Text(
+                       "Containers!",
+                       style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 25
+                       )
+                   ),
+              ),)
             ],
           )
     )

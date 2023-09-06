0d24f452f8452b2e99cdd4d65249edbbfa6ceaaa
RaT332 <rahultekchandani323@gmail.com>
2023-09-06 19:21:32 +0530

    Drawer with Items Example.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -12,23 +12,24 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            title: const Text("Flutter"),
-            actions: const [
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-                child: Icon(Icons.info),
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: ListView(
+            children:  [
+              ListTile(
+                leading: const Icon(Icons.people),
+                title: const Text("Item 1"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.train),
+                title: const Text("Item 2"),
+                onTap: () {},
               )
-            ]
+            ],
+          ),
         ),
-        drawer: const Drawer(),
-        body: const Center(
-          child: Text("Wow nice book"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
+        endDrawer: const Drawer(),
       ),
     );
   }
